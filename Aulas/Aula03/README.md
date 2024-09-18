5a604f999dd77788623b046f3b55cda3e76257cf
gvheisler <gvheisler@inf.ufsm.br>
2024-09-18 13:12:31 -0300

    update

--- Aulas/Aula03/README.md
+++ Aulas/Aula03/README.md
@@ -1,1 +1,5 @@
 # Aula 03
+
+## Material auxiliar
+
+[Funções estatísticas úteis](https://ashki23.github.io/r-functions.html)
--- Aulas/Aula03/exemplo_vendas.R
+++ Aulas/Aula03/exemplo_vendas.R
@@ -2,9 +2,13 @@ df <- read.csv(url("https://gvheisler.github.io/ds/vendas.csv"))
 
 df$Data <- as.Date(df$Data, format = "%Y-%d-%m")
 
-plot(x = df$Data[which(df$Produto=="Produto A")], y = df$Vendas[which(df$Produto=="Produto A")], type = 'l', ylim = c(50,200))
-lines(x = df$Data[which(df$Produto=="Produto B")], y = df$Vendas[which(df$Produto=="Produto B")], col = 'red')
-lines(x = df$Data[which(df$Produto=="Produto C")], y = df$Vendas[which(df$Produto=="Produto C")], col = 'blue')
+plot(x = df$Data[which(df$Produto=="Produto A")], 
+     y = df$Vendas[which(df$Produto=="Produto A")], 
+     type = 'l', ylim = c(50,200))
+lines(x = df$Data[which(df$Produto=="Produto B")],
+      y = df$Vendas[which(df$Produto=="Produto B")], col = 'red')
+lines(x = df$Data[which(df$Produto=="Produto C")], 
+      y = df$Vendas[which(df$Produto=="Produto C")], col = 'blue')
 
 library(dplyr)
 
